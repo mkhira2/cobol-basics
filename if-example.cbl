5558360a94eecf3fa5966e10806e948b322c57ed
Kenji Hirabayashi <mkhira2@gmail.com>
2018-11-11 08:54:29 -0600

    if example

---
+++ if-example.cbl
@@ -0,0 +1,18 @@
+       IDENTIFICATION DIVISION.
+       AUTHOR. kenjihirabayashi
+       PROGRAM-ID. IF-EXAMPLE.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 UserInput PIC X(20).
+
+       PROCEDURE DIVISION.
+       DISPLAY 'Please enter your name in upper-case: '.
+       ACCEPT UserInput.
+       IF UserInput IS ALPHABETIC-LOWER
+           MOVE FUNCTION UPPER-CASE (UserInput) TO UserInput
+           DISPLAY "That's not uppercase, but I fixed it, ", UserInput
+           ELSE DISPLAY 'Hello, ', UserInput
+
+        END-IF
+        STOP RUN.
