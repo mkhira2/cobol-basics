9dd6381835efccabf2d7b6be5f8b9b6cbaa2876a
Kenji Hirabayashi <mkhira2@gmail.com>
2018-11-11 08:39:58 -0600

    hello world

---
+++ hello-world.cbl
@@ -0,0 +1,7 @@
+      * Prints a simple string
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-WORLD.
+       AUTHOR. kenjihirabayashi
+       PROCEDURE DIVISION.
+       DISPLAY 'Hello World'.
+       STOP RUN.
